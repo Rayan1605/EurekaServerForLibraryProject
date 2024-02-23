e29c5bdbff301a3fcba81038d73df71a6d081d63
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-02-23 01:44:52 +0000

    Start

---
+++ src/test/java/com/example/eurekaprojectforlibraryproject/EurekaProjectForLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaprojectforlibraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaProjectForLibraryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
